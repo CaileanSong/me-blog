45e4ca8c30335194a68d9f0759043bf5987c8915
song <songyunfeifly@163.com>
2025-04-09 17:14:34 +0800

    fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,13 +4,14 @@ import matter from 'gray-matter';
 import { remark } from 'remark';
 import remarkHtml from 'remark-html';
 
+// 声明 PageProps 的类型，params 直接是一个包含 slug 字符串的对象
 interface PageProps {
     params: {
         slug: string;
     };
-    // 其他可能的属性...
 }
-// 获取存放 Markdown 文件的路径
+
+// 获取 Markdown 文件所在的路径
 const postsDirectory = path.join(process.cwd(), 'posts');
 
 // 根据 slug 获取文章内容
@@ -22,22 +23,23 @@ async function getPostData(slug: string) {
 
     const processedContent = remark().use(remarkHtml).processSync(content);
     const contentHtml = processedContent.toString();
-    console.log(contentHtml)
+
     return {
         contentHtml,
         ...data,
     };
 }
 
-// 使用 `fetch` 或 `Promise` 获取静态数据
+// 页面组件，params 作为参数传递，异步获取文章内容
 export default async function Post({ params }: PageProps) {
+    // 使用 params.slug 获取文章内容
     const postData = await getPostData(params.slug);
 
     return (
         <div>
-            <h1 className='text-3xl font-bold mb-4 text-light-text'># {postData.title}</h1>
-            <p className='text-light-time'>{postData.date}</p>
-            <div className='mt-8 text-light-body prose' dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <h1 className="text-3xl font-bold mb-4 text-light-text"># {postData.title}</h1>
+            <p className="text-light-time">{postData.date}</p>
+            <div className="mt-8 text-light-body prose" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </div>
     );
 }
